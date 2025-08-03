306155d92dd0133f544e95ef34b748cda7116de4
Straple <hadayegor@gmail.com>
2025-08-03 22:45:28 +0300

    hotfix

--- src/settings.hpp
+++ src/settings.hpp
@@ -2,13 +2,6 @@
 
 #include <cstdint>
 
-// Compile-time settings now controlled by CMake options:
-// -DENABLE_ROTATE_MODEL=ON/OFF
-// -DENABLE_ASSERT=ON/OFF
-// -DENABLE_SCHEDULER_CHANGE_TASK=ON/OFF
-// -DEPIBT_DEPTH=<value>
-// -DTHREADS_NUM_DEFAULT=<value>
-
 // Convert macro to constexpr
 
 constexpr uint32_t THREADS_NUM_VALUE = THREADS_NUM;
